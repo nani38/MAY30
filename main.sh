37ea793f0dca34007406724d8407f9c68bf64723
testuser1 <testuser1@ip-192-168-1-100.us-east-2.compute.internal>
2021-05-29 18:02:35 +0000

    added users in the machine

--- main.sh
+++ main.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "this script will provide info abt the local system"
+echo
+#gives hostname
 echo $(hostname)
+echo
+#gives user executed this script
 echo $(whoami)
-echo $(id -un)
+echo
+#gives users in the system
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS 
